23cbfd689d09c147e639f836e41f4003336574f3
Jolyn Tellis <jolyntellis@gmail.com>
2024-11-10 18:02:18 +0000

    Updated the layout of the page. A design issues need to be addressed.

--- js/main.js
+++
@@ -1,6 +0,0 @@
-var toggleSwitch = document.querySelector(".toggle-switch");
-
-toggleSwitch.addEventListener("click", () => {
-    document.body.classList.toggle('dark-theme');
-    toggleSwitch.classList.toggle('active');
-});
